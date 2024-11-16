bddbebc0fb91d0b033f76701811d49b6fb453131
jordanhemingway-lccc <jordanhemingway@student.lccc.wy.edu>
2024-11-16 03:56:49 +0000

    Submitting homework assignment for OOP Unit.

--- 5_Make_Folder/main.cpp
+++ 5_Make_Folder/main.cpp
@@ -21,14 +21,35 @@ int main() {
    Triangle triangle1;
    Triangle triangle2;
 
+   double userBase, userHeight;
+
    // TODO: Read and set base and height for triangle1 (use SetBase() and SetHeight())
-      
+   cout << "Please enter the base for triangle 1." << endl;
+   cin >> userBase;
+   cout << "Please enter the height for triangle 1." << endl;
+   cin >> userHeight;
+   triangle1.SetBase(userBase);
+   triangle1.SetHeight(userHeight);
+
    // TODO: Read and set base and height for triangle2 (use SetBase() and SetHeight())
-       
+   cout << "Please enter the base for triangle 2." << endl;
+   cin >> userBase;
+   cout << "Please enter the height for triangle 2." << endl;
+   cin >> userHeight;
+   triangle2.SetBase(userBase);
+   triangle2.SetHeight(userHeight);
+
    cout << "Triangle with smaller area:" << endl;
    
    // TODO: Determine smaller triangle (use GetArea())  
    //       and output smaller triangle's info (use PrintInfo())
-   
+
+   if (triangle1.GetArea() < triangle2.GetArea()) {
+      triangle1.PrintInfo();
+   }
+   else {
+      triangle2.PrintInfo();
+   }
+
    return 0;
 }
